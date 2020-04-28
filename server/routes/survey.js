340958574f007c6d42e4119f3eaf291877153905
Bappaditya <bappaditya.sasmal@qlikchain.com>
2020-04-28 12:30:12 +0530

    Query modified

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -70,65 +70,20 @@ router.route('/')
 router.route('/:surveyid')
 .get(async (req, res) => {
     debug(`GET: /surveys/${req.params.surveyid} ;`);
+    const surveyid = req.params.surveyid;
 
     try {
-        const rows = await config.knex("surveys as s")
-        .leftJoin("questions as q", "s.id", "q.survey_id")
-        .leftJoin("question_option as qo", "q.id", "qo.question_id")
-        .leftJoin("options as o", "qo.option_id", "o.id")
-        .where("s.id", req.params.surveyid)
-        .select("s.id as id", "s.name as name", "s.description as description", "version", "status",
-        "s.created_at as created_at", "locked_at", "s.created_by as created_by", "q.id as question_id",
-        "q.description as question_description", "note", "mandatory", "q.type as question_type",
-        "attachments", "q.created_at as question_created_at", "o.id as option_id", "value",
-        "o.description as option_description", "o.type as option_type", "o.survey_id as option_survey_id")
-        .orderBy("s.id", "q.id", "o.id");
+        const questions =  config.knex("questions as q")
+        .leftJoin("question_option as qo","q.id","qo.question_id")
+        .leftJoin("options as o","qo.option_id","o.id")
+        .select("q.*",config.knex.raw("to_json(array_remove(array_agg(o),NULL)) \"options\""))
+        .groupBy("q.id");
 
-        const surveys = [];
-        let survey    = {},
-            question  = {};
-
-        rows.forEach(row => {
-            if(row.id !== survey.id) {
-                survey = {
-                    id: row.id,
-                    name: row.name,
-                    description: row.description,
-                    version: row.version,
-                    status: row.status,
-                    created_at: row.created_at,
-                    locked_at: row.locked_at,
-                    created_by: row.created_by,
-                    questions: []
-                }
-                surveys.push(survey);
-            }
-
-            if(row.question_id !== question.id) {
-                question = {
-                    id: row.question_id,
-                    description: row.question_description,
-                    note: row.note,
-                    mandatory: row.mandatory,
-                    type: row.question_type,
-                    attachments: row.attachments,
-                    created_at: row.question_created_at,
-                    options: []
-                }
-                survey.questions.push(question);
-            }
-
-            if(row.option_id) {
-                option = {
-                    id: row.option_id,
-                    value: row.value,
-                    description: row.option_description,
-                    type: row.option_type,
-                    survey_id: row.option_survey_id
-                }
-                question.options.push(option);
-            }
-        });
+        const surveys = await config.knex("surveys as s")
+        .leftJoin(questions.as("q"),"s.id","q.survey_id")
+        .select("s.*", config.knex.raw("to_json(array_remove(array_agg(q),NULL)) questions"))
+        .where("s.id",surveyid)
+        .groupBy("s.id");
 
         if(surveys.length > 0) {
             return res.status(200).send(surveys[0]);
